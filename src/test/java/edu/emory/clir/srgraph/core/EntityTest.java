f02aff61b87f9c46fb4e8e61e704e979023f48eb
Tomasz Jurczyk <dzordzyk@gmail.com>
2015-07-14 16:33:16 -0400

    Tests to be written.

---
+++ src/test/java/edu/emory/clir/srgraph/core/EntityTest.java
@@ -0,0 +1,33 @@
+package edu.emory.clir.srgraph.core;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * @author Tomasz Jurczyk ({@code tomasz.jurczyk@emory.edu})
+ * @since 1.0
+ */
+public class EntityTest {
+
+    @Test
+    public void test()
+    {
+
+    }
+
+    public void testAddInstance() throws Exception {
+
+    }
+
+    public void testAddInstances() throws Exception {
+
+    }
+
+    public void testGetInstanceSet() throws Exception {
+
+    }
+
+    public void testRemoveInstance() throws Exception {
+
+    }
+}
---
+++ src/test/java/edu/emory/clir/srgraph/core/InstanceTest.java
@@ -0,0 +1,11 @@
+package edu.emory.clir.srgraph.core;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Tomasz Jurczyk ({@code tomasz.jurczyk@emory.edu})
+ * @since 1.0
+ */
+public class InstanceTest {
+
+}
